c5bcae40a1437afe2ac4815cee867409b006045f
ephron <maheshbabuece143@gmail.com>
2022-11-03 06:14:34 -0600

    First Commit

---
+++ Test Git/src/Test1/Test.java
@@ -0,0 +1,24 @@
+package Test1;
+
+public class Test {
+	
+	String A = "Hello World MAhesh";
+	
+	
+	public void MA() {
+		
+		System.out.println("The output is:"+ A);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Test Obj = new Test();
+		Obj.MA();
+	
+		
+
+	}
+
+}
